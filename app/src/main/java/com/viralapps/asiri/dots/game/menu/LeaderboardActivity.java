f3b40a52b75051fd15eb0f1ce3073c7e5618d43b
asirihewage <foolmashi@gmail.com>
2020-09-04 18:39:35 +0530

    Global user rank

--- app/src/main/java/com/viralapps/asiri/dots/game/menu/LeaderboardActivity.java
+++ app/src/main/java/com/viralapps/asiri/dots/game/menu/LeaderboardActivity.java
@@ -20,167 +20,93 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 import com.viralapps.asiri.dots.game.R;
+import com.viralapps.asiri.dots.game.User;
 import com.viralapps.asiri.dots.game.UserListAdapter;
 import com.viralapps.asiri.dots.game.UserSettings;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LeaderboardActivity extends Activity {
     private Context mContext;
-    private View mRootView;
-    private ImageView mLogo;
-    private Button mPlayGameSingle;
-    private Button leaderboard;
-    private Button mUnlockMoreStuff;
-    private Button mCheckMeOut;
+    private View leaderboardviewRootView;
+    private View leaderboardview;
     private TextView mFastestScore;
-    private TextView mFastestScore1;
-    private TextView mFastestScore2;
-    private TextView mFastestScore3;
-    private ImageButton mSound;
-    private ImageButton mVibrate;
-    private ImageButton mNightMode;
-    private int[] mScore1Set;
-    private int[] mScore2Set;
-    private int[] mScore3Set;
     private boolean mIsGraphLaunch;
     private boolean mIsSoundOn;
-    private boolean mIsVibrateOn;
     private boolean mIsNightMode;
-    private int mTextColor;
     private SharedPreferences mUserSettings;
-    private DatabaseReference devicesRef;
-    private String deviceID;
+    private DatabaseReference usersRef;
     private TextView usernameTxt;
     private ListView userlistView;
     private String username;
     private String dotsPerSecond;
+    private ListView list;
+    TextView highesrmark;
+    ProgressBar progressBar;
+    private String TAG;
+    UserListAdapter adapter;
 
-    @SuppressLint("CutPasteId")
+    @SuppressLint({"CutPasteId", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leaderboard_activity);
         mContext = this;
+        TAG = "LeaderBoardActivity";
 
-        AdView mAdView = new AdView(this);
-        mAdView.setAdUnitId("ca-app-pub-1300983540101573/7550736743");
-        mAdView.setAdSize(AdSize.BANNER);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
+//        AdView mAdView = new AdView(this);
+//        mAdView.setAdUnitId("ca-app-pub-1300983540101573/7550736743");
+//        mAdView.setAdSize(AdSize.BANNER);
+//        AdRequest adRequest = new AdRequest.Builder().build();
+//        mAdView.loadAd(adRequest);
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        devicesRef = database.getReference("devices");
-
-        ListView list;
-
-//        // Read from the database
-//        devicesRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                // This method is called once with the initial value and again
-//                // whenever data at this location is updated.
-////                String value = dataSnapshot.getValue(String.class);
-////                Log.d(TAG, "Value is: " + value);
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                // Failed to read value
-////                Log.w(TAG, "Failed to read value.", error.toException());
-//            }
-//        });
-
-        String[] maintitle ={
-                "Title 1","Title 2",
-                "Title 3","Title 4",
-                "Title 5","Title 1","Title 2",
-                "Title 3","Title 4",
-                "Title 5",
-        };
-
-        String[] subtitle ={
-                "Sub Title 1","Sub Title 2",
-                "Sub Title 3","Sub Title 4",
-                "Sub Title 5","Sub Title 1","Sub Title 2",
-                "Sub Title 3","Sub Title 4",
-                "Sub Title 5",
-        };
-
-        Integer[] imgid={
-                R.drawable.logo,R.drawable.ic_dot_sprite_blue,
-                R.drawable.ic_dot_sprite_blue,R.drawable.ic_dot_sprite_blue,
-                R.drawable.ic_dot_sprite_blue,R.drawable.ic_dot_sprite_blue,
-                R.drawable.ic_dot_sprite_blue,
-                R.drawable.ic_dot_sprite_blue,R.drawable.ic_dot_sprite_blue,
-                R.drawable.ic_dot_sprite_blue,
-        };
-
-        UserListAdapter adapter=new UserListAdapter(this, maintitle, subtitle,imgid);
-        list=(ListView)findViewById(R.id.userlistView);
-        list.setAdapter(adapter);
-
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
-                // TODO Auto-generated method stub
-                if(position == 0) {
-                    //code specific to first list item
-                    Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        usersRef = database.getReference("users");
 
         enableImmersiveMode();
 
         Vibrator vb = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
-        mFastestScore = findViewById(R.id.fastest_score);
         usernameTxt = findViewById(R.id.username);
         userlistView = findViewById(R.id.userlistView);
 
-        mTextColor = getColor(R.color.primaryTextColor);
-
         UserSettings.startMenuMusic(this);
-
-        mLogo = findViewById(R.id.logo);
 
         mUserSettings = this.getSharedPreferences("user_settings", Context.MODE_PRIVATE);
         mIsSoundOn = mUserSettings.getBoolean("sound_on", true);
         UserSettings.setSoundEnable(mIsSoundOn);
-        mIsVibrateOn = mUserSettings.getBoolean("vibrate_on", true);
         mIsNightMode = mUserSettings.getBoolean("night_mode", false);
-        deviceID = mUserSettings.getString("deviceID", "");
         username = mUserSettings.getString("name", "");
-        dotsPerSecond = mUserSettings.getString("dotsPerSecond", "0");
+        dotsPerSecond = mUserSettings.getString("dotsPerSecond", "0.0");
         usernameTxt.setText(username);
+
+        updateColors();
 
         Snackbar.make(userlistView,"Hi, " + username, Snackbar.LENGTH_SHORT)
                 .show();
@@ -190,31 +116,106 @@ public class LeaderboardActivity extends Activity {
         String imageUri = mUserSettings.getString("avatar", "");
         ImageView useravatar = findViewById(R.id.useravatar);
         Picasso.get().load(imageUri).into(useravatar);
-        TextView highesrmark = findViewById(R.id.highestMark);
-        highesrmark.setText(dotsPerSecond + " Dots per second.");
-
-
+        highesrmark = findViewById(R.id.highestMark);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
 
         /*
-         * Create a DatabaseReference to the data; works with standard DatabaseReference methods
-         * like limitToLast() and etc.
+         * create user list
          */
-        devicesRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                    String dotsPerSecond = Objects.requireNonNull(ds.child("dotsPerSecond").getValue()).toString();
-                    //etc
-                    Log.w("dfdgggggggggggggggggg", ds.toString());
+        ArrayList <User> userArr = new ArrayList<>();
+
+        usersRef.orderByChild("dotsPerSecond").addChildEventListener(new ChildEventListener() {
+
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot snapshot, String prevChildKey) {
+//                    User newUser = snapshot.getValue(User.class);
+                    String avatar = snapshot.child("avatar").getValue().toString();
+                    String deviceID = snapshot.child("deviceID").getValue().toString();
+                    String dps = snapshot.child("dotsPerSecond").getValue().toString();
+                    String email = snapshot.child("email").getValue().toString();
+                    String scoreSet = snapshot.child("scoreSet").getValue().toString();
+                    String totalTime = snapshot.child("totalTime").getValue().toString();
+                    String userID = snapshot.child("userID").getValue().toString();
+                    String name = snapshot.child("name").getValue().toString();
+                    String timestamp = snapshot.child("timestamp").getValue().toString();
+
+//                    Log.e(TAG,avatar);
+                    User usr = new User(avatar, deviceID, dps, email, name, scoreSet, timestamp, totalTime, userID);
+                    userArr.add(usr);
+                    setList(userArr);
+                    progressBar.setVisibility(View.GONE);
                 }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, String prevChildKey) {
+                    String avatar = dataSnapshot.child("avatar").getValue().toString();
+                    String deviceID = dataSnapshot.child("deviceID").getValue().toString();
+                    String dps = dataSnapshot.child("dotsPerSecond").getValue().toString();
+                    String email = dataSnapshot.child("email").getValue().toString();
+                    String scoreSet = dataSnapshot.child("scoreSet").getValue().toString();
+                    String totalTime = dataSnapshot.child("totalTime").getValue().toString();
+                    String userID = dataSnapshot.child("userID").getValue().toString();
+                    String name = dataSnapshot.child("name").getValue().toString();
+                    String timestamp = dataSnapshot.child("timestamp").getValue().toString();
 
-            }
-        });
+                    User usr = new User(avatar, deviceID, dps, email, name, scoreSet, timestamp, totalTime, userID);
+                    int u = userArr.indexOf(usr);
+                    Log.e(TAG, usr.toString());
+                    adapter.notifyDataSetChanged();
+                }
 
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                    String avatar = dataSnapshot.child("avatar").getValue().toString();
+                    String deviceID = dataSnapshot.child("deviceID").getValue().toString();
+                    String dps = dataSnapshot.child("dotsPerSecond").getValue().toString();
+                    String email = dataSnapshot.child("email").getValue().toString();
+                    String scoreSet = dataSnapshot.child("scoreSet").getValue().toString();
+                    String totalTime = dataSnapshot.child("totalTime").getValue().toString();
+                    String userID = dataSnapshot.child("userID").getValue().toString();
+                    String name = dataSnapshot.child("name").getValue().toString();
+                    String timestamp = dataSnapshot.child("timestamp").getValue().toString();
+
+                    User usr = new User(avatar, deviceID, dps, email, name, scoreSet, timestamp, totalTime, userID);
+                    userArr.remove(usr);
+                    adapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, String prevChildKey) {}
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {}
+            });
+
+    }
+
+    private void setList(ArrayList<User> userList){
+        list=(ListView)findViewById(R.id.userlistView);
+        List<String> name = new ArrayList<>();
+        List<String> highScore = new ArrayList<>();
+        List<String> imgid = new ArrayList<>();
+        List<String> scoreSet = new ArrayList<>();
+
+        for(User user: userList) {
+            name.add(user.getName());
+            highScore.add(user.gethighScore());
+            imgid.add(user.getAvatar());
+            scoreSet.add(user.getScoreSet());
+        }
+
+        String[] nameArray = new String[ name.size() ];
+        name.toArray( nameArray );
+        String[] highScoreArray = new String[ highScore.size() ];
+        highScore.toArray( highScoreArray );
+        String[] scoreSetArray = new String[ scoreSet.size() ];
+        scoreSet.toArray( scoreSetArray );
+        String[] imgidArray = new String[ imgid.size() ];
+        imgid.toArray( imgidArray );
+
+        adapter = new UserListAdapter(this, nameArray, highScoreArray, imgidArray, scoreSetArray);
+        list.setAdapter(adapter);
     }
 
     @Override
@@ -245,11 +246,29 @@ public class LeaderboardActivity extends Activity {
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         // Set the content to appear under the system bars so that the
                         // content doesn't resize when the system bars hide and show.
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
+
+    private void updateColors() {
+        leaderboardview = findViewById(R.id.userlistView);
+        View usercard = findViewById(R.id.usercard);
+        TextView userName = findViewById(R.id.username);
+        TextView userscore = findViewById(R.id.highestMarkUser);
+
+        leaderboardviewRootView = leaderboardview.getRootView();
+
+        if (mIsNightMode) {
+            userscore.setTextColor(Color.BLACK);
+            userName.setTextColor(Color.BLACK);
+            usercard.setBackgroundColor(Color.WHITE);
+            leaderboardviewRootView.setBackgroundColor(Color.BLACK);
+        } else {
+            userscore.setTextColor(Color.WHITE);
+            userName.setTextColor(Color.WHITE);
+            usercard.setBackgroundColor(Color.BLACK);
+            leaderboardviewRootView.setBackgroundColor(Color.WHITE);
+
+        }
+    }
+
 }
